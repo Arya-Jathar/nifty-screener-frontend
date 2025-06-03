eaa6405d12e0d616183558a663b1df350084f265
Arya-Jathar <167429418+Arya-Jathar@users.noreply.github.com>
2025-06-03 14:27:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
       if (portfolio.length === 0) return;
       const tickers = portfolio.map(p => p.ticker).join(',');
       try {
-        const res = await fetch(`https://nifty-screener-backend.onrender.com`);
+        const res = await fetch(`https://nifty-screener-backend.onrender.com/get_prices?tickers=${tickers}`);
         const data = await res.json();
         setLivePrices(data);
       } catch (err) {
@@ -50,7 +50,7 @@ function App() {
   const fetchStockData = async () => {
     if (!selectedStock) return alert("Please select a stock!");
     try {
-      const res = await fetch(`https://nifty-screener-backend.onrender.com`);
+      const res = await fetch(`https://nifty-screener-backend.onrender.com/get_prices?tickers=${tickers}`);
       const data = await res.json();
       setStockData(data);
     } catch (err) {
